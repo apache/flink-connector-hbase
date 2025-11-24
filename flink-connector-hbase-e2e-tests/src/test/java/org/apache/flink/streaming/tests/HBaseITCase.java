fce04d8611cef721529aaa7a67f1546058ef2e80
mateczagany <czmate10@gmail.com>
2025-11-24 11:34:31 +0100

    [FLINK-38722] Upgrade HBase to 2.6.4

--- flink-connector-hbase-e2e-tests/src/test/java/org/apache/flink/streaming/tests/HBaseITCase.java
+++ flink-connector-hbase-e2e-tests/src/test/java/org/apache/flink/streaming/tests/HBaseITCase.java
@@ -51,7 +51,7 @@ import static org.testcontainers.shaded.org.hamcrest.Matchers.containsString;
 /** End to end HBase connector tests. */
 class HBaseITCase {
 
-    private static final String HBASE_VERSION = "2.6.3";
+    private static final String HBASE_VERSION = "2.6.4";
     private static final String CONNECTOR_VERSION = "hbase-2.6";
     private static final String HBASE_E2E_SQL = "hbase_e2e.sql";
     private static final Path HADOOP_CP = ResourceTestUtils.getResource(".*hadoop.classpath");
